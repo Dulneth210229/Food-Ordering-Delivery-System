6960fd9d3d8da4037787b96aa0d86d35fc2c4432
Mona Lisa <oberathna2020@gmail.com>
2025-04-18 11:43:02 +0530

    Create Order Backend

---
+++ services/order-service/orderBackEnd/routes/orderRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const orderController = require('../controllers/orderController');
+
+router.post('/', orderController.createOrder);
+router.get('/', orderController.getOrders);
+router.put('/:id', orderController.updateOrder);
+router.put('/:id/confirm', orderController.confirmOrder);
+
+module.exports = router;
