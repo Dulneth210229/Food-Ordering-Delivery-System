33b0ad916da393be27289e566f44407df65867a5
Mona Lisa <oberathna2020@gmail.com>
2025-04-27 10:01:30 +0530

    Create Dockerfile

--- services/order-service/order-service.js
+++ services/order-service/order-service.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 
 // Routes
 app.use('/api/orders', orderRoutes);
